a0c9047a2c16ac04d4246b65f262a61c1f595473
Hemant Prajapati <hemantprajapati20500@gmail.com>
2022-07-20 19:43:28 +0530

    day2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import { HomepageTop } from './Components/Homepage/HomepageTop';
+import { Mid } from './Components/Homepage/Mid';
 
 
 function App() {
   return (
    <div>
   <Navbar/>
- 
+ <HomepageTop/>
+ <Mid/>
    </div>
   );
 }
