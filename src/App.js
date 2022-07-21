b33765f5407c5e8da7168e0bf3292c4662200d22
Hemant Prajapati <hemantprajapati20500@gmail.com>
2022-07-21 21:30:33 +0530

    day3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/Homepage/Navbar';
 import { HomepageTop } from './Components/Homepage/HomepageTop';
 import { Mid } from './Components/Homepage/Mid';
+import { Login } from './Components/Sign&Login/Login';
+import { DetailForm } from './Components/Sign&Login/DetailForm';
+import { BookingCars } from './Components/Booking Cars/BookingCars';
 
 
 function App() {
   return (
    <div>
-  <Navbar/>
- <HomepageTop/>
- <Mid/>
+  {/* <Login/> */}
+  {/* <DetailForm/> */}
+  <BookingCars/>
    </div>
   );
 }
---
+++ src/Components/Booking Cars/BookingCars.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const BookingCars = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
