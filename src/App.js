aedb9ae52fd66651f9f50df03cbb7549859c7f0c
Hemant Prajapati <hemantprajapati20500@gmail.com>
2022-07-22 23:15:38 +0530

    day4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
-import Navbar from './Components/Homepage/Navbar';
+
 import { HomepageTop } from './Components/Homepage/HomepageTop';
 import { Mid } from './Components/Homepage/Mid';
 import { Login } from './Components/Sign&Login/Login';
 import { DetailForm } from './Components/Sign&Login/DetailForm';
 import { BookingCars } from './Components/Booking Cars/BookingCars';
+import { CarDetails } from './Components/Data/CarDetails';
+
 
 
 function App() {
@@ -13,7 +15,9 @@ function App() {
    <div>
   {/* <Login/> */}
   {/* <DetailForm/> */}
+  
   <BookingCars/>
+  {/* <CarDetails/> */}
    </div>
   );
 }
